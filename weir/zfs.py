81774b776cbaba4ad1bfda8ff7f7340ac5f0ffe8
Stephen Drake <steve@synergyconsultingnz.com>
2015-03-15 15:10:15 +1300

    Make subprocess streams consistent between Python 2 & 3
    Use automatic buffering (bufsize=-1) by default in both Python 2 & 3,
    and always return streams opened with the io module.  The subprocess
    module in Python 2 uses open() rather than io.open() and the resulting
    streams cannot be wrapped with TextIOWrapper.  This is necessary for
    reading text from stderr when stdin or stdout is a binary stream (eg
    zfs send and receive).

--- weir/zfs.py
+++ weir/zfs.py
@@ -1,10 +1,18 @@
 import errno
+import io
 import logging
+import os
 import re
 import subprocess
 import threading
 
 log = logging.getLogger(__name__)
+
+# Reopen file with new mode, buffer size etc using io module
+def reopen(file, *args, **kwargs):
+	fd = os.dup(file.fileno())
+	file.close()
+	return io.open(fd, *args, **kwargs)
 
 # Variant of os.popen() that works on an existing process
 def popen(p):
@@ -57,7 +65,8 @@ class Process(subprocess.Popen):
 	# XXX: this is used for DEVNULL in Python 3
 	STDERR = STDOUT - 1         # -3
 
-	def __init__(self, cmd, stdin=None, stdout=None, stderr=None, **kwargs):
+	def __init__(self, cmd, stdin=None, stdout=None, stderr=None,
+			bufsize=-1, universal_newlines=False, **kwargs):
 		# use stderr=STDOUT to combine streams for stdout=STDERR
 		redir_stdout = (stdout == Process.STDERR)
 		if redir_stdout:
@@ -65,12 +74,28 @@ class Process(subprocess.Popen):
 
 		# initialise process
 		super(Process, self).__init__(
-			cmd, stdin=stdin, stdout=stdout, stderr=stderr, **kwargs)
+			cmd, stdin=stdin, stdout=stdout, stderr=stderr,
+			bufsize=bufsize, universal_newlines=universal_newlines, **kwargs)
 		self.cmd = cmd
 
 		# move output to stderr for stdout=STDERR
 		if redir_stdout:
 			self.stdout, self.stderr = None, self.stdout
+
+		# reopen standard streams for consistency between Python 2 & 3
+		if self.stdin and not isinstance(self.stdin, io.IOBase):
+			self.stdin = reopen(self.stdin, 'wb', bufsize)
+			if universal_newlines:
+				self.stdin = io.TextIOWrapper(self.stdin,
+					write_through=True, line_buffering=(bufsize == 1))
+		if self.stdout and not isinstance(self.stdout, io.IOBase):
+			self.stdout = reopen(self.stdout, 'rb', bufsize)
+			if universal_newlines:
+				self.stdout = io.TextIOWrapper(self.stdout)
+		if self.stderr and not isinstance(self.stderr, io.IOBase):
+			self.stderr = reopen(self.stderr, 'rb', bufsize)
+			if universal_newlines:
+				self.stderr = io.TextIOWrapper(self.stderr)
 
 	def check(self):
 		if self.returncode:
