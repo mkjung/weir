c6df1022fae8919ed002713c3e435e128baa2290
Stephen Drake <steve@synergyconsultingnz.com>
2015-08-28 20:33:08 +1200

    Created version 0.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='weir',
-    version='0.4.0.dev',
+    version='0.4.0',
     description='A Python API for ZFS',
     long_description=long_description,
 
