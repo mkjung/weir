a6e8053ed0938c4b4cdbf5b6e9e6d4844dc883e4
Hajo M?ller <dasjoe@gmail.com>
2016-05-08 20:13:59 +0000

    Overwrote __repr__ for ZFS datasets so "print zfs.find('pool')" is human-readable

--- weir/zfs.py
+++ weir/zfs.py
@@ -164,6 +164,9 @@ class ZFSDataset(object):
 	def __str__(self):
 		return self.name
 
+	def __repr__(self):
+		return self.name
+
 	def parent(self):
 		parent_name, _, _ = self.name.rpartition('/')
 		return open(parent_name) if parent_name else None
